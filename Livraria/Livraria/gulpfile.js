f5d9fa25738673a8c579f8c4da92a7591c75dbb7
Vinicius Trindade <viniciustrin@live.com>
2018-04-01 18:30:22 -0300

    setup de configuraçoes

--- Livraria/Livraria/gulpfile.js
+++ Livraria/Livraria/gulpfile.js
@@ -1,2 +1,42 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], { read: false });
+
+    var injectOptions = {
+        ignorePath:'/public'
+    };
+ 
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('restarting');
+        });
+});
