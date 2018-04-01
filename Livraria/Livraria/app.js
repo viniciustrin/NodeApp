289c4813490e35734e5488fae7cdc63de12f124f
Vinicius Trindade <viniciustrin@live.com>
2018-04-01 10:57:32 -0300

    initial commit

---
+++ Livraria/Livraria/app.js
@@ -0,0 +1,17 @@
+﻿//C:\Users\Vinicius\source\repos\NodeApp\Livraria\Livraria
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+var port = 5000;
+app.listen(port, function (err) {
+    console.log("app started at port: " + port);
+});
+
+app.get('/', function (req,res) {
+});
